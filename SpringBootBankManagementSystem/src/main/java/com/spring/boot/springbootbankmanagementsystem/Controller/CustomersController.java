060d1d9a5db2c226a1739bc6f981e244871aff51
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-07-17 23:00:48 +0300

    Work in progress . . .

--- SpringBootBankManagementSystem/src/main/java/com/spring/boot/springbootbankmanagementsystem/Controller/CustomersController.java
+++ SpringBootBankManagementSystem/src/main/java/com/spring/boot/springbootbankmanagementsystem/Controller/CustomersController.java
@@ -1,14 +1,69 @@
 package com.spring.boot.springbootbankmanagementsystem.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.spring.boot.springbootbankmanagementsystem.Api.ApiResponse;
+import com.spring.boot.springbootbankmanagementsystem.Model.Customer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomersController {
-//    Create Bank management system where you can get all the Customers
-//  , add , remove , update Customers
-//    Customers Class : ID , Username , Balance
+    //    Create Bank management system where you can
+    ArrayList<Customer> customers = new ArrayList<>();
+    int id = 100;
+
+    //    get all the Customers
+    @GetMapping("/list")
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    //  , add
+    @PostMapping("/register")
+    public ApiResponse addCustomer(@RequestBody Customer customer) {
+        customer.setID(Integer.toString(id));
+        id++;
+        customers.add(customer);
+
+        return new ApiResponse("Customer registered successfully", "200");
+    }
+
+    //  , remove
+    @DeleteMapping("/delete/{iD}")
+    public ApiResponse deleteCustomer(@PathVariable String iD) {
+        boolean deleted = false;
+        for (Customer customer : customers) {
+            if (customer.getID().equals(iD)) {
+                customers.remove(customer);
+                deleted = true;
+                break;
+            }
+        }
+        if (deleted) {
+            return new ApiResponse("Customer was deleted successfully", "200 OK");
+        } else {
+            return new ApiResponse("Error: the customer does not exist", "404 not found");
+        }
+    }
+
+    //  , update Customers
+    @PutMapping("/update/{iD}")
+    public ApiResponse updateCustomer(@PathVariable String iD, @RequestBody Customer requestedCustomer) {
+        boolean updated = false;
+        for (Customer customer : customers) {
+            if (customer.getID().equals(iD)) {
+                customers.set(customers.indexOf(customer),requestedCustomer);
+                updated = true;
+                break;
+            }
+        }
+        if (updated) {
+            return new ApiResponse("Customer was updated successfully", "200 OK");
+        } else {
+            return new ApiResponse("Error: the customer does not exist", "404 not found");
+        }
+    }
 //    Endpoints :
 //    Get all the customers
 //    Add new customers
--- SpringBootBankManagementSystem/src/main/java/com/spring/boot/springbootbankmanagementsystem/Model/Customer.java
+++ SpringBootBankManagementSystem/src/main/java/com/spring/boot/springbootbankmanagementsystem/Model/Customer.java
@@ -1,4 +1,11 @@
 package com.spring.boot.springbootbankmanagementsystem.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Customer {
+    private String iD, username;
+    private double balance;
 }
