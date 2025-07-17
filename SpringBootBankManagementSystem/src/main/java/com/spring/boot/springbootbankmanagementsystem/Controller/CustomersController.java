95ee5b096dff11eee09fa63fddacf1f069d60f8b
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-07-17 23:22:03 +0300

    Done, testing in progress

--- SpringBootBankManagementSystem/src/main/java/com/spring/boot/springbootbankmanagementsystem/Controller/CustomersController.java
+++ SpringBootBankManagementSystem/src/main/java/com/spring/boot/springbootbankmanagementsystem/Controller/CustomersController.java
@@ -9,17 +9,14 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomersController {
-    //    Create Bank management system where you can
     ArrayList<Customer> customers = new ArrayList<>();
     int id = 100;
 
-    //    get all the Customers
     @GetMapping("/list")
     public ArrayList<Customer> getCustomers() {
         return customers;
     }
 
-    //  , add
     @PostMapping("/register")
     public ApiResponse addCustomer(@RequestBody Customer customer) {
         customer.setID(Integer.toString(id));
@@ -29,7 +26,6 @@ public class CustomersController {
         return new ApiResponse("Customer registered successfully", "200");
     }
 
-    //  , remove
     @DeleteMapping("/delete/{iD}")
     public ApiResponse deleteCustomer(@PathVariable String iD) {
         boolean deleted = false;
@@ -47,13 +43,12 @@ public class CustomersController {
         }
     }
 
-    //  , update Customers
     @PutMapping("/update/{iD}")
     public ApiResponse updateCustomer(@PathVariable String iD, @RequestBody Customer requestedCustomer) {
         boolean updated = false;
         for (Customer customer : customers) {
             if (customer.getID().equals(iD)) {
-                customers.set(customers.indexOf(customer),requestedCustomer);
+                customers.set(customers.indexOf(customer), requestedCustomer);
                 updated = true;
                 break;
             }
@@ -64,11 +59,53 @@ public class CustomersController {
             return new ApiResponse("Error: the customer does not exist", "404 not found");
         }
     }
-//    Endpoints :
-//    Get all the customers
-//    Add new customers
-//    Update customers
-//    Delete customers
-//    Deposit money to customer
-//    Withdraw money from customers
+
+    @PutMapping("/deposit/{iD}/{amount}")
+    public ApiResponse deposit(@PathVariable double amount, @PathVariable String iD) {
+        if (amount <= 0) {
+            return new ApiResponse("Error: the amount can not be zero or negative", "400 Bad Request");
+        }
+        boolean deposited = false;
+        for (Customer customer : customers) {
+            if (customer.getID().equals(iD)) {
+                customers.get(customers.indexOf(customer)).setBalance(
+                        customers.get(customers.indexOf(customer)).getBalance() + amount
+                );
+                deposited = true;
+                break;
+            }
+        }
+        if (deposited) {
+            return new ApiResponse("Customer deposited " + amount + " successfully", "200 OK");
+        } else {
+            return new ApiResponse("Error: the customer does not exist", "404 not found");
+        }
+    }
+
+    @PutMapping("/withdraw/{iD}/{amount}")
+    public ApiResponse withdraw(@PathVariable double amount, @PathVariable String iD) {
+        if (amount <= 0) {
+            return new ApiResponse("Error: the amount can not be zero or negative", "400 Bad Request");
+        }
+        boolean withdrawn = false;
+        for (Customer customer : customers) {
+            if (customer.getID().equals(iD)) {
+                if (customers.get(customers.indexOf(customer)).getBalance() < amount) {
+                    return new ApiResponse("Error: Not enough balance", "400 Bad Request");
+                } else {
+                    customers.get(customers.indexOf(customer)).setBalance(
+                            customers.get(customers.indexOf(customer)).getBalance() - amount
+                    );
+                    withdrawn = true;
+                }
+                break;
+            }
+        }
+        if (withdrawn) {
+            return new ApiResponse("Customer withdrawn " + amount + " successfully", "200 OK");
+        } else {
+            return new ApiResponse("Error: the customer does not exist", "404 not found");
+        }
+
+    }
 }
